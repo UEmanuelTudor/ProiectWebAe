b351f33456d277e296c61e6b837809275da03112
UEmanuelT <uifalean_e@yahoo.com>
2023-01-15 13:48:02 +0200

    test

--- Models/Marca.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProiectAEWeb.Models
-{
-    public class Marca
-    {
-        public int ID { get; set; }
-        public string NumeMarca { get; set; }
-        public ICollection<Serviciu>? Servicii { get; set; } //navigation property
-    }
-}
